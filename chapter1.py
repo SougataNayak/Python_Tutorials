232dc1758980965b4d8fb3db3cf9a9d90166d70b
sougata-nayak <sougatanayak007@gmail.com>
2021-03-28 12:11:53 +0530

    chapter 1

---
+++ chapter1.py
@@ -0,0 +1,18 @@
+# Modules, comments and pip
+
+import os
+print("This is how to use external modules ", os.listdir())
+# Here os is a built-in module in python, module is like a library which we can import and use
+# There are also many external modules which can be installed and then used, pip is used to install those modules
+
+# This is a single line comment
+''' This is a multi line comment 
+and can be spread to any number of lines'''
+
+# Also python single line codes can be run in REPL (Read, Execute, Print, Loop)
+
+print('\nThis is how to print single line strings')
+print('''This is 
+how to print
+multiple line strings''')
+
